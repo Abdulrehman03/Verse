ce84c8c6ab6c818ca088a2c0541fbda4d7d11585
Berlin <Berlin@Apples-MacBook-Pro.local>
2022-01-20 01:10:03 +0500

    changes added

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,32 +1,26 @@
-import * as React from "react";
-import { createStyles, makeStyles } from "@mui/styles";
-import Layout from "../Components/Layout/Layout";
-import MainPage from "../Components/Landing/MainPage";
-import DetailsPage from "../Components/Landing/DetailsPage";
-import { ShowDetailContext } from "../Context/ShowDetail";
+import * as React from 'react';
+import { createStyles, makeStyles } from '@mui/styles';
+import Layout from '../Components/Layout/Layout';
+import MainPage from '../Components/Landing/MainPage';
+import DetailsPage from '../Components/Landing/DetailsPage';
+import { ShowDetailContext } from '../Context/ShowDetail';
 
 const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      display: "flex",
-      alignItems: "center",
-      height: "75vh",
-      width: "100%",
-    },
-  })
+    createStyles({
+        root: {
+            display: 'flex',
+            alignItems: 'center',
+            height: '75vh',
+            width: '100%',
+        },
+    }),
 );
 
 const Landing = () => {
-  const classes = useStyles();
-  const { showDetails } = React.useContext(ShowDetailContext);
+    const classes = useStyles();
+    const { showDetails } = React.useContext(ShowDetailContext);
 
-  return (
-    <Layout>
-      <div className={classes.root}>
-        {showDetails === true ? <DetailsPage /> : <MainPage />}
-      </div>
-    </Layout>
-  );
+    return <Layout>{showDetails === true ? <DetailsPage /> : <MainPage />}</Layout>;
 };
 
 export default Landing;
